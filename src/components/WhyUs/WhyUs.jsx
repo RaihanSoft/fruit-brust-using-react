3f048fd39441bb2cb25566b7602202f9a0ff64f5
Abu Raihan <137011693+RaihanSoft@users.noreply.github.com>
2024-09-02 18:00:25 +0600

    Why Choose Us section added

--- src/components/WhyUs/WhyUs.jsx
+++ src/components/WhyUs/WhyUs.jsx
@@ -1,10 +1,21 @@
 function WhyUs() {
     return (
         <div className='w-11/12 mx-auto'>
-            <section>
+            {/* description section  */}
+            <div className="text-center my-10">
+                <h1 className="text-4xl font-bold py-2">Why Choose Us</h1>
+                <p>Contrary to popular belief, is not simply random text. It has roots in a piece of classical <br /> Latin literature from 45 BC, making it over 2000 years old.</p>
+            </div>
+
+
+
+
+
+            {/* content section  */}
+            <section className="grid grid-cols-3">
                 {/* one  */}
-                <div>
-                    <div className="flex gap-2 border-4 w-1/3 p-4">
+                <div className="space-y-10">
+                    <div className="flex gap-2 p-4">
                         <div className="flex items-center justify-center text-xl  ">
                             <i class="ri-group-fill"></i>
                         </div>
@@ -18,7 +29,7 @@ function WhyUs() {
                     </div>
 
 
-                    <div className="flex gap-2 border-4 w-1/3 p-4">
+                    <div className="flex gap-2 p-4">
                         <div className="flex items-center justify-center text-xl  ">
                             <i class="ri-group-fill"></i>
                         </div>
@@ -31,7 +42,7 @@ function WhyUs() {
 
                     </div>
 
-                    <div className="flex gap-2 border-4 w-1/3 p-4">
+                    <div className="flex gap-2  p-4">
                         <div className="flex items-center justify-center text-xl  ">
                             <i class="ri-group-fill"></i>
                         </div>
@@ -45,9 +56,57 @@ function WhyUs() {
                     </div>
                 </div>
 
-                <div></div>
+                <div>
+                    <img src="./assets/whychoose1.png" alt="" />
+                </div>
 
-                <div></div>
+                <div className="space-y-10">
+                    {/* one  */}
+                    <div className="flex gap-2 p-4">
+
+                        <div className="text-right">
+                            <h1 className="font-bold text-xl">Dedicated Team</h1>
+                            <p>Professional employees are there for you to pick <br /> the most amazing and fresh fruits.</p>
+                        </div>
+
+                        <div className="flex items-center text-xl  ">
+                            <i class="ri-group-fill"></i>
+                        </div>
+
+
+                    </div>
+                    {/* one  */}
+                    <div className="flex gap-2 p-4">
+
+                        <div className="text-right">
+                            <h1 className="font-bold text-xl">Dedicated Team</h1>
+                            <p>Professional employees are there for you to pick <br /> the most amazing and fresh fruits.</p>
+                        </div>
+
+                        <div className="flex items-center text-xl  ">
+                            <i class="ri-group-fill"></i>
+                        </div>
+
+
+                    </div>
+                    {/* one  */}
+                    <div className="flex gap-2 p-4">
+
+                        <div className="text-right">
+                            <h1 className="font-bold text-xl">Dedicated Team</h1>
+                            <p>Professional employees are there for you to pick <br /> the most amazing and fresh fruits.</p>
+                        </div>
+
+                        <div className="flex items-center text-xl  ">
+                            <i class="ri-group-fill"></i>
+                        </div>
+
+
+                    </div>
+
+
+
+                </div>
 
             </section>
         </div>
